8c55538a094125baddacdc01bf304f223268af5a
mike dupont <mike.dupont@introspector.local>
2024-11-25 15:37:17 -0500

    all ips
    all ips

---
+++ test.py
@@ -0,0 +1,15 @@
+from openai import OpenAI
+client = OpenAI(
+    base_url='http://localhost:11434/v1/',
+    api_key='ollama',  # required but ignored
+)
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            'role': 'user',
+            'content': 'Say this is a test',
+        }
+    ],
+    model='qwen2.5-coder',
+)
+print(chat_completion)
