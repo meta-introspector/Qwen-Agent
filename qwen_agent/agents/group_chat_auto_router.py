3db6738f5603e6215b4c39db59d390e694b7087f
mike dupont <mike.dupont@introspector.local>
2024-11-26 07:39:13 -0500

    now forcing bots to answer

--- qwen_agent/agents/group_chat_auto_router.py
+++ qwen_agent/agents/group_chat_auto_router.py
@@ -17,16 +17,16 @@ class GroupChatAutoRouter(Agent):
 # 请阅读对话历史，并选择下一个合适的发言角色，从 [{agent_names}] 里选，当真实用户最近表明了停止聊天时，或话题应该终止时，请返回“[STOP]”，用户很懒，非必要不要选真实用户。
 # 仅返回角色名或“[STOP]”，不要返回其余内容。'''
 
-    PROMPT_TEMPLATE_EN = '''You are in a role play game. The following roles are available:
+    PROMPT_TEMPLATE_EN = '''You are in a competitive battle royal team deathmatch role play game. The following roles are available:
 {agent_descs}
 
 The format of dialogue history between roles is as follows:
 Role Name: Speech Content
 
-Please read the dialogue history and choose the next suitable role to speak.
-When the user indicates to stop chatting or when the topic should be terminated, please return '[STOP]'.
-Only return the role name from [{agent_names}] or '[STOP]'. Do not reply any other content.'''
+Please read the dialogue history and choose the next suitable role to invoke, like the avatar you will emerge into the world to save the day.
+Only return the role name from [{agent_names}] or '[STOP]'. '''
 
+#    When the user indicates to stop chatting or when the topic should be terminated, please return '[STOP]'.
     PROMPT_TEMPLATE = {
         'zh': PROMPT_TEMPLATE_EN,
         'en': PROMPT_TEMPLATE_EN,
